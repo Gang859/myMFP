9a60706e9e5e45ef853eeeff659635fa3acdb7e8
Gang859 <zhangrg107@outlook.com>
2025-02-21 10:49:42 +0800

    update eval

--- run/train_and_eval.sh
+++ run/train_and_eval.sh
@@ -1,6 +1,7 @@
 train_start_month="01"
 train_end_month="05"
 test_end_month="06"
+output_file="/mnt/zhangrengang/workspace/myMFP/output/submission_1.csv"
 
 # full dataset
 python /mnt/zhangrengang/workspace/myMFP/src/baseline.py \
@@ -9,13 +10,13 @@ python /mnt/zhangrengang/workspace/myMFP/src/baseline.py \
   --train_data_path /home/zhangrengang/Doc/release_features_${train_start_month}-${test_end_month}/train_data \
   --test_data_path /home/zhangrengang/Doc/release_features_${train_start_month}-${test_end_month}/test_data \
   --ticket_path /home/zhangrengang/Doc/competition_data/stage1_feather/ticket.csv \
-  --output_file /mnt/zhangrengang/workspace/myMFP/output/submission_1.csv \
+  --output_file  ${output_file} \
   --s1_train_start_month ${train_start_month} \
   --s1_train_end_month ${train_end_month} \
   --s1_test_end_month ${test_end_month} \
   > /mnt/zhangrengang/workspace/myMFP/run_log/baseline.log
 
 python /mnt/zhangrengang/workspace/myMFP/src/evaluator.py \
-  --submission_file /mnt/zhangrengang/workspace/myMFP/output/submission_1.csv \
+  --submission_file ${output_file} \
   --cutoff_date ${train_end_month} \
   > /mnt/zhangrengang/workspace/myMFP/run_log/eval.log
